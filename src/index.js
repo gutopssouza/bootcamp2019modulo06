31b31cfc0ba27280af0bb3f4eed32fd81592e0eb
Augusto <gutopssouza@gmail.com>
2019-08-07 19:49:15 -0300

    :rocket: Configure navitagion using react native gesture handler and react native navitagion libs :rocket: Create main and user pages :rocket: Implement routes navigation for user and main pages :rocket: Create Main header

--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-console.tron.log('Hello world');
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native</Text>
-      <Text style={styles.welcome}>Abaixo 02</Text>
-    </View>
-  );
+  return <Routes />;
 }
